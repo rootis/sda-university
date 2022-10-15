e613a5423e334cadf3a319cf92e66b56057730d2
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2022-10-15 11:40:24 +0300

    Universities endpoint implementation completed.

--- src/main/java/com/sdacademy/university/controller/UniversityController.java
+++ src/main/java/com/sdacademy/university/controller/UniversityController.java
@@ -1,7 +1,9 @@
 package com.sdacademy.university.controller;
 
 import com.sdacademy.university.model.entity.UniversityEntity;
+import com.sdacademy.university.service.UniversityService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/universities")
 public class UniversityController {
 
+    @Autowired
+    private UniversityService universityService;
+
     @GetMapping
     public List<UniversityEntity> getUniversities() {
-        // TBD: complete implementation
-        return null;
+        return universityService.getUniversities();
     }
 }
